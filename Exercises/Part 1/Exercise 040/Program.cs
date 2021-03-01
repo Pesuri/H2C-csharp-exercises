daa2ff81b8bd2d2451c4bf52d44030b269ee0bfb
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:32:12 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -5,9 +5,18 @@ namespace exercise_40
   class Program
   {
     public static void Main(string[] args)
-    {
-
-      // Write your code here:
+    { 
+        int summa = 0;
+        int laskuri = 0;
+        while (true)
+        {         
+            Console.WriteLine("Give a number:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if      (numero == 0)   {break;}
+            else                    {summa += numero; laskuri += 1;}
+        }
+        Console.WriteLine("Total sum of numbers: " + summa); 
+        Console.WriteLine("Total amount of numbers: " + laskuri); 
     }
   }
 }
