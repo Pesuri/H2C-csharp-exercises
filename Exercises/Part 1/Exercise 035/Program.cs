e8e3f948a9877ad71256eaf22998417b348360b3
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:17:42 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 035/Program.cs
+++ Exercises/Part 1/Exercise 035/Program.cs
@@ -6,10 +6,12 @@ namespace exercise_35
   {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Give a number:");
-        int numero = Convert.ToInt32(Console.ReadLine());
-        if (numero % 2 == 1) {Console.WriteLine("It is odd.");}
-        else                {Console.WriteLine("It is even.");}
+        while (true)
+        {         
+            Console.WriteLine("Give a number:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero == 42) {break;}
+        } 
     }
   }
 }
