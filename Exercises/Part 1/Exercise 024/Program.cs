69a4fafcd4a7ffd774728339be09df66750dc7c8
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:29:20 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -6,9 +6,9 @@ namespace exercise_24
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
-      
+        Console.WriteLine("Your speed:");
+        int nopeus = Convert.ToInt32(Console.ReadLine());
+        if (nopeus > 120) {Console.WriteLine("Speeding!");}
     }
   }
 }
