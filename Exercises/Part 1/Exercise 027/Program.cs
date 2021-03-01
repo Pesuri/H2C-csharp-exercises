38a92c894d512943c0d2b3261d3ec3dec7108fa9
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:37:39 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,7 +6,10 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        Console.WriteLine("Give a number:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero < 0) {Console.WriteLine("It is not positive");}
+        else            {Console.WriteLine("It is positive");}
 
     }
   }
