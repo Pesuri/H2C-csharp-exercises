55d36d207a897855da9b700e7ee5ef90eb2b5454
Jussi <jussiottopesonen@gmail.com>
2021-03-01 15:37:18 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,7 +6,11 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("Hello, how are you?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("That's interesting, tell me more");
+            answer = Console.ReadLine();
+            Console.WriteLine("Thank you for sharing!");
 
         }
     }
