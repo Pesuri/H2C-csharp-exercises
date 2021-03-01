6cdc46e8ed2ceb7d3425dade269c8c043d2a0790
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:16:22 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 034/Program.cs
+++ Exercises/Part 1/Exercise 034/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_34
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        while (true)
+        {
+            Console.WriteLine("Do you want to continue?");
+            string vastaus = Console.ReadLine();
+            if (vastaus == "no") {break;}
+        }          
     }
   }
 }
--- Exercises/Part 1/Exercise 035/Program.cs
+++ Exercises/Part 1/Exercise 035/Program.cs
@@ -6,8 +6,10 @@ namespace exercise_35
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        Console.WriteLine("Give a number:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero % 2 == 1) {Console.WriteLine("It is odd.");}
+        else                {Console.WriteLine("It is even.");}
     }
   }
 }
