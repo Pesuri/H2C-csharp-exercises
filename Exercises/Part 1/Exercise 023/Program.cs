cde6db91d1f8b0eeca55f9f7eda2d5ca1d6afe3d
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:25:11 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -6,9 +6,19 @@ namespace exercise_23
   {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Give the first number!");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number!");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
 
-      // Write your code here:
-
+        int summa = numero1 + numero2;
+        int erotus = numero1 - numero2;
+        int tulo = numero1 * numero2;
+        double jako = numero1 / (numero2 + 0.0);
+        Console.WriteLine(numero1 + " + " + numero2 + " = " + summa);
+        Console.WriteLine(numero1 + " - " + numero2 + " = " + erotus);
+        Console.WriteLine(numero1 + " * " + numero2 + " = " + tulo);
+        Console.WriteLine(numero1 + " / " + numero2 + " = " + jako);
     }
   }
 }
