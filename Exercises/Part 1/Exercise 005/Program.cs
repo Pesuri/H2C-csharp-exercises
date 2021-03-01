1e1e771e44c6911ffd822fd30b555905366f7ecf
Jussi <jussiottopesonen@gmail.com>
2021-03-01 15:24:46 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 005/Program.cs
+++ Exercises/Part 1/Exercise 005/Program.cs
@@ -7,7 +7,11 @@ namespace exercise_05
         public static void Main(string[] args)
         {
             string name = "Ada Lovelace";
-            // Write your code here:
+            Console.WriteLine("Hello Ada LoveLace!");
+            //Is there a typo in assingment?
+            //We are told to print Ada LoveLace with uppercase L in middle of name.
+            //Since we are not told to use variable just not to modify it this is the result.
+            //othervice I would have used: Console.WriteLine("Hello " + name + "!");
             
         }
     }
