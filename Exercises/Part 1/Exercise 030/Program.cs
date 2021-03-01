68b992c05c99711f37e117062e6a2a797e8e3632
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:53:25 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,16 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        Console.WriteLine("Give your percent [0 - 100]:");
+        int prosentti = Convert.ToInt32(Console.ReadLine());
+        if      (prosentti < 0)     {Console.WriteLine("Impossible"     );}
+        else if (prosentti <= 49)   {Console.WriteLine("Fail"           );}
+        else if (prosentti <= 59)   {Console.WriteLine("Grade: 1"       );}
+        else if (prosentti <= 69)   {Console.WriteLine("Grade: 2"       );}
+        else if (prosentti <= 79)   {Console.WriteLine("Grade: 3"       );}
+        else if (prosentti <= 89)   {Console.WriteLine("Grade: 4"       );}
+        else if (prosentti <= 100)  {Console.WriteLine("Grade: 5"       );}
+        else                        {Console.WriteLine("Outstanding!"   );}
     }
   }
 }
