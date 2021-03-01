b8636e00ff8eec33a61a05188b7dca148d0cb7d2
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:08:27 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -6,8 +6,14 @@ namespace exercise_18
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give the first number!");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number!");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the third number!");
+        int numero3 = Convert.ToInt32(Console.ReadLine());
+        int summa = numero1 + numero2 + numero3;
+        Console.WriteLine("The sum is " + summa);
 
     }
   }
