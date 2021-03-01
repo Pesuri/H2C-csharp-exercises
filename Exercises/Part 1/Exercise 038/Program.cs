f93eedd8b26cde4cbbcc2fb058e94e7f03ae86af
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:27:33 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,8 +6,15 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        int laskuri = 0;
+        while (true)
+        {         
+            Console.WriteLine("Give a number:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if      (numero == 0)   {break;}
+            else if (numero < 0)    {laskuri += 1;}
+        }
+        Console.WriteLine("Total amount of negative numbers: " + laskuri);
     }
   }
 }
