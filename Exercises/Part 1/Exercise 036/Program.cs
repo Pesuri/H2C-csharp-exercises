31b2fda96bce99fc46c8f5ba3fc5093869de13ae
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:21:18 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,15 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        while (true)
+        {         
+            Console.WriteLine("Give a number:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int poweroftwo = numero * numero;
+            if      (numero == 0)   {break;}
+            else if (numero < 0)    {Console.WriteLine("That is negative");}
+            else                    {Console.WriteLine(poweroftwo);}
+        } 
     }
   }
 }
