887505cc620ee90fe6f569b289540306d1ab7a34
Jussi <jussiottopesonen@gmail.com>
2021-03-01 22:09:11 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -6,8 +6,10 @@ namespace exercise_32
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+        Console.WriteLine("Speak, friend, and enter!"); //Nice Lord of the Rings reference.
+        string vastaus = Console.ReadLine();
+        if (vastaus == "Mellon")    {Console.WriteLine("Welcome, friend");}
+        else                        {Console.WriteLine("They've got a cave troll!");}
     }
   }
 }
