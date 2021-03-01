1464a7b11401ac6ea3fa4c930be1cdd594892d6e
Jussi <jussiottopesonen@gmail.com>
2021-03-01 20:16:50 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -6,9 +6,12 @@ namespace exercise_21
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
-
+        Console.WriteLine("Give the first number!");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number!");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+        double keskiarvo = (numero1 + numero2) / 2.0;
+        Console.WriteLine("The average is " + keskiarvo);
     }
   }
 }
