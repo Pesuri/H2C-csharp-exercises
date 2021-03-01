d2b5ce9cdfffd41a2d584a534f6dfa5c0eefa515
Jussi <jussiottopesonen@gmail.com>
2021-03-01 19:58:19 +0200

    1.3.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -6,8 +6,18 @@ namespace exercise_15
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give a string:");
+        string  mjono = Console.ReadLine();
+        Console.WriteLine("Give a integer:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give a double:");
+        double numberd = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Give a boolean:");
+        Boolean truth = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Your string: " + mjono);
+        Console.WriteLine("Your integer: " + number);
+        Console.WriteLine("Your double: " + numberd);
+        Console.WriteLine("Your boolean: " + truth);
 
     }
   }
