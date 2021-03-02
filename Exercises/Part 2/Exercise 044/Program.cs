7c32fc635a421f88a3e6f5ce4ee56c447d43296e
Jussi <jussiottopesonen@gmail.com>
2021-03-02 13:58:11 +0200

    2.3.2021

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -6,9 +6,12 @@ namespace exercise_44
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
-
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+        if      (numero1 > numero2) {Console.WriteLine(numero1 + " is grEater than " + numero2);} //I wonder what is the joke with writing greater as grEater.
+        else if (numero1 < numero2) {Console.WriteLine(numero1 + " is less than " + numero2);}
+        else                        {Console.WriteLine(numero1 + " is equal to " + numero2);}
+        
     }
   }
 }
