55145290403eec9f8f706a17510bcbf7e951d0c2
Jussi <jussiottopesonen@gmail.com>
2021-03-02 14:59:34 +0200

    2.3.2021

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -2,14 +2,16 @@
 
 namespace exercise_52
 {
+  class Program
+  {
     public static void Main(String[] args)
     {
-        PrintFromNumberToOne(-5);
+        PrintFromNumberToOne(10);
     }
 
     public static void PrintFromNumberToOne(int number)
     {
-        if (number > 1)
+        if (number <= 1)
         {
             for (int i = number; i <= 1; i++){Console.WriteLine(i);}
         }
