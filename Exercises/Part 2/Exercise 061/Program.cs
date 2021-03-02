9c0df40b2b0cd44c0912dd546e14edd28aa7c122
Jussi <jussiottopesonen@gmail.com>
2021-03-02 17:00:18 +0200

    2.3.2021

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,47 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+        ChristmasTree(10);
     }
 
     public static void PrintStars(int number)
     {
-
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
     }
 
     public static void PrintSpaces(int number)
     {
-
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write(" ");
+        }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+        for (int i = 0; i < size; i++)
+        {
+            PrintSpaces(size-(i+1));
+            PrintStars(1+i);
+        }
     }
 
     public static void ChristmasTree(int height)
     {
-
+        for (int i = 0; i < height; i++)
+        {
+            PrintSpaces(height - (i + 1));
+            PrintStars(1 + 2 * i);
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            PrintSpaces(height - 2);
+            PrintStars(3);
+        }
     }
   }
 }
