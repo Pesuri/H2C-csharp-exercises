ca5fb2858804463e4415ec1c762f5697c3e54fa9
Jussi <jussiottopesonen@gmail.com>
2021-03-02 14:27:13 +0200

    2.3.2021

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,30 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+        int numero = 0;
+        int summa = 0;
+        int laskuri = 0;
+        int pariton = 0;
+        Console.WriteLine("Give numers:");
+        while(numero != -1)
+        {
+            numero = Convert.ToInt32(Console.ReadLine()); 
+            if (numero != -1)
+            {
+                summa += numero;
+                laskuri++;
+                if (numero % 2 == 1) {pariton++;}
+            }
+        }
+        double average = summa / (laskuri + 0.0);
+        int parillinen = laskuri - pariton;
+        Console.WriteLine("Thx! Bye!");
+        Console.WriteLine("Sum: " + summa);
+        Console.WriteLine("Numbers: " + laskuri);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Even: " + parillinen);
+        Console.WriteLine("Odd: " + pariton);
 
-      // Write your code here:
- 
     }
   }
 }
