81c6568534b35ac469216867a60b537c2bf9cb7d
Jussi <jussiottopesonen@gmail.com>
2021-03-02 16:24:49 +0200

    2.3.2021

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,16 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = GrEatest(2, 7, 3);
       Console.WriteLine("Greatest: " + answer);
     }
 
-    // Write your method here:
+    public static int GrEatest(int number1, int number2, int number3)
+    {
+        if      (number1 > number2 && number1 > number3){return number1;}
+        else if (number2 > number1 && number2 > number3){return number2;}
+        else                                            {return number3;}
+    }
 
   }
 }
