115b4c760acd1c75e3e3702c279e0c148dfbb955
Jussi <jussiottopesonen@gmail.com>
2021-03-08 12:35:17 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 069/Program.cs
+++ Exercises/Part 3/Exercise 069/Program.cs
@@ -7,17 +7,24 @@ namespace exercise_69
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
+        List<int> list = new List<int>();
+        while (true)
+        {
         int input = Convert.ToInt32(Console.ReadLine());
         if (input == -1)
         {
-          break;
+            break;
         }
         list.Add(input);
-      }
-
+        }
+        Console.WriteLine("From where?");
+        int start = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Where to?");
+        int end = Convert.ToInt32(Console.ReadLine());
+        foreach(int i in list)
+        {
+            if(i > start & i < end){Console.WriteLine(i);}
+        }
     }
   }
 }
