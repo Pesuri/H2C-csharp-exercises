8491cc9d9da0379308d5da681fe0f7f17e08d138
Jussi <jussiottopesonen@gmail.com>
2021-03-08 16:14:41 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+        string input = "1";
+        while(input != "")
+        {
+            input = Console.ReadLine();
+            if(input != "")
+            {
+                string[] split = input.Split(' ');
+                for(int i = 0; i < split.Length; i++)
+                {
+                    if(split[i].Contains("av")){Console.WriteLine(split[i]);}
+                }
 
+            }
+        }
     }
 
   }
