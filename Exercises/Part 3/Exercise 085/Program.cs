04e6352bab2eefd575f92fbc6ea62dc3d99449dc
Jussi <jussiottopesonen@gmail.com>
2021-03-08 14:55:12 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,8 +7,21 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
-
-
+        string[,] login = new string[2,2] {{"alex","sunshine"},{"emma","haskell"}};
+        Console.WriteLine("Enter username: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password: ");
+        string password = Console.ReadLine();
+        int loggedin = 0;
+        for(int i = 0; i < 2; i++)
+        {
+            if(login[i,0] == username && login[i,1] == password)
+            {
+                Console.WriteLine("You have succesfully logged in!");
+                loggedin = 1;
+            }
+        }
+        if(loggedin == 0){Console.WriteLine("Incorrect username or password!");}
     }
   }
 }
