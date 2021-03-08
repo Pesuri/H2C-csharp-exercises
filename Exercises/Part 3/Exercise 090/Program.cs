76f98fd32e80ecf06d82ea5abe20ec0df5c8a2b2
Jussi <jussiottopesonen@gmail.com>
2021-03-08 16:57:01 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 090/Program.cs
+++ Exercises/Part 3/Exercise 090/Program.cs
@@ -7,8 +7,26 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
-
-
+        string input = "1";
+        List<string> names = new List<string>();
+        List<string> ages = new List<string>();
+        while(input != "")
+        {
+            input = Console.ReadLine();
+            if(input != "")
+            {
+                string[] split = input.Split(',');
+                names.Add(split[0]);
+                ages.Add(split[1]);
+            }
+            
+        }
+        int oldest = Convert.ToInt32(ages[0]);
+        foreach(string age in ages)
+        {
+            if(Convert.ToInt32(age) > oldest){oldest = Convert.ToInt32(age);}
+        }
+        Console.WriteLine("Age of the oldest: " + oldest);
     }
   }
 }
