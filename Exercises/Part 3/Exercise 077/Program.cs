966ad5adb1a89050252cbaae956bbb6d6524071f
Jussi <jussiottopesonen@gmail.com>
2021-03-08 13:44:23 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 077/Program.cs
+++ Exercises/Part 3/Exercise 077/Program.cs
@@ -7,7 +7,32 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
-      // You can try your method here if you want
+        List<string> strings = new List<string>();
+
+        strings.Add("First");
+        strings.Add("Second");
+        strings.Add("Third");
+
+        // Remember, this is how you print all the items in a list
+        strings.ForEach(Console.WriteLine);
+        Console.WriteLine();
+
+        RemoveLast(strings);
+        strings.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        RemoveLast(strings);
+
+        strings.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        RemoveLast(strings);
+        strings.ForEach(Console.WriteLine);
+    }
+    public static void RemoveLast(List<string> strings)
+    {
+        if(strings.Count != 0)
+        {
+            strings.RemoveAt(strings.Count-1);
+        }
     }
 
   }
