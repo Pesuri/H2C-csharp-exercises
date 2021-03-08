069d48fe775ed31230970a68fed65f5f6ce737a5
Jussi <jussiottopesonen@gmail.com>
2021-03-08 17:11:14 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,7 +7,38 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+        string input = "1";
+        int currentYear = 2020;
+        List<string> names = new List<string>();
+        List<string> years = new List<string>();
+        while(input != "")
+        {
+            input = Console.ReadLine();
+            if(input != "")
+            {
+                string[] split = input.Split(',');
+                names.Add(split[0]);
+                years.Add(split[1]);
+            }
+            
+        }
+        int oldestAge = currentYear - Convert.ToInt32(years[0]);
+        int longestNameInt = names[0].Length;
+        string longestName = names[0];
+        for(int i = 0; i < years.Count; i++)
+        {
+            if(currentYear - Convert.ToInt32(years[i]) > oldestAge)
+            {
+                oldestAge = currentYear - Convert.ToInt32(years[i]);
+            }
+            if(longestNameInt < names[i].Length)
+            {
+                longestNameInt = names[i].Length;
+                longestName = names[i];
+            }
+        }
+        Console.WriteLine("Longest name: " + longestName);
+        Console.WriteLine("Highest age: " + oldestAge);
     }
   }
 }
