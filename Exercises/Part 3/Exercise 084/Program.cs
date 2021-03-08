7697e65bc2255f953d0f73dd955c1a2e248126af
Jussi <jussiottopesonen@gmail.com>
2021-03-08 14:41:00 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 084/Program.cs
+++ Exercises/Part 3/Exercise 084/Program.cs
@@ -7,8 +7,14 @@ namespace exercise_84
   {
     public static void Main(string[] args)
     {
-
-
+        string input = "";
+        while(input != "true")
+        {
+            Console.Write("Give a string: ");
+            input = Console.ReadLine();
+            if(input != "true"){Console.WriteLine("Try again!");}
+        }
+        Console.WriteLine("You got it right!");
     }
   }
 }
