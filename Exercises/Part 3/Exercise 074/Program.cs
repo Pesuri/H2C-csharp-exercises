a645a60b62cf15af2bc69ac59e4d38a089f99525
Jussi <jussiottopesonen@gmail.com>
2021-03-08 13:30:11 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 074/Program.cs
+++ Exercises/Part 3/Exercise 074/Program.cs
@@ -7,17 +7,20 @@ namespace exercise_74
   {
     public static void Main(string[] args)
     {
-      List<string> list = new List<string>();
-      while (true)
-      {
-        string input = Console.ReadLine();
-        if (input == "")
+        List<string> list = new List<string>();
+        while (true)
         {
-          break;
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+                break;
+            }
+            list.Add(input);
         }
-        list.Add(input);
-      }
-      
+        Console.WriteLine("Search for?");
+        string search = Console.ReadLine();
+        if(list.Contains(search))   {Console.WriteLine(search +" was found!");}
+        else                        {Console.WriteLine(search +" was not found!");}
     }
   }
 }
