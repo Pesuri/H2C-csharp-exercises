b4d1aa98a907c7b4493f1918817cc8418b3cf62c
Jussi <jussiottopesonen@gmail.com>
2021-03-08 12:58:33 +0200

    8.3.2021

--- Exercises/Part 3/Exercise 071/Program.cs
+++ Exercises/Part 3/Exercise 071/Program.cs
@@ -7,16 +7,30 @@ namespace exercise_71
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        List<int> list = new List<int>();
+        while (true)
         {
-          break;
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == -1)
+            {
+                break;
+            }
+            list.Add(input);
         }
-        list.Add(input);
-      }
+        Console.WriteLine("Search for?");
+        int search = Convert.ToInt32(Console.ReadLine());
+        List<int> searchResults = new List<int>();
+        if(list.Contains(search))
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                if(list[i] == search){searchResults.Add(i);}
+            }       
+            foreach(int i in searchResults)
+            {
+                Console.WriteLine(search + " is at index " + i);
+            }
+        }
 
     }
   }
