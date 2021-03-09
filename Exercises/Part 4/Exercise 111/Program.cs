356bf8bdafb43b520f024d0cc7fe02b33b8a4cb6
Jussi <jussiottopesonen@gmail.com>
2021-03-09 16:54:00 +0200

    9.3.2021

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -7,9 +7,29 @@ namespace exercise_111
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      List<PersonalInformation> list = new List<PersonalInformation>();
+        List<PersonalInformation> list = new List<PersonalInformation>();
 
+        while(true)
+        {
+            Console.WriteLine("First name: ");
+            string firstname = Console.ReadLine();
+            
+            if(firstname == "") {break;}
+
+            Console.WriteLine("Last name: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Identification number: ");
+            string identification = Console.ReadLine();
+
+            list.Add(new PersonalInformation(firstname, lastname, identification));
+        }
+
+        Console.WriteLine();
+
+        foreach(PersonalInformation person in list)
+        {
+            Console.WriteLine(person.firstName + " " + person.lastName);
+        }
     }
   }
 }
