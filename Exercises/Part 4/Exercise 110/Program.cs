9d9d01d82aa03a1fba2b915db30efe7a16fa519a
Jussi <jussiottopesonen@gmail.com>
2021-03-09 16:36:56 +0200

    9.3.2021

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -7,7 +7,15 @@ namespace exercise_110
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        List<Item> items = new List<Item>();
+        while(true)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            if(name == "") {break;}
+            items.Add(new Item(name));
+        }
+        items.ForEach(Console.WriteLine);
     }
   }
 }
