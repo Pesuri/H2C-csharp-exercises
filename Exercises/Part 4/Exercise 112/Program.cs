09b62e2abf63b86d159333fe01969937fda819db
Jussi <jussiottopesonen@gmail.com>
2021-03-09 17:04:32 +0200

    9.3.2021

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -7,7 +7,30 @@ namespace exercise_112
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        List<TelevisionProgram> list = new List<TelevisionProgram>();
+
+        while(true)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            
+            if(name == "") {break;}
+
+            Console.Write("Duration: ");
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            list.Add(new TelevisionProgram(name, duration));
+        }
+        Console.WriteLine();
+        
+        Console.Write("Program's maximum duration? ");
+        int max = Convert.ToInt32(Console.ReadLine());
+
+        foreach(TelevisionProgram program in list)
+        {
+            if(program.duration <= max) { Console.WriteLine(program); }    
+        }
+
 
 
     }
