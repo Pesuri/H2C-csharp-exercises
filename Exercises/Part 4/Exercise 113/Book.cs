6341e72293270e0c89d7385a88b87a2472d8316e
Jussi <jussiottopesonen@gmail.com>
2021-03-09 17:26:48 +0200

    9.3.2021

---
+++ Exercises/Part 4/Exercise 113/Book.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace exercise_113
+{
+    public class Book
+    {
+        public string name;
+        public int year;
+        public int pages;
+
+        public Book(string name, int pages, int year)
+        {
+            this.name = name;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public override string ToString()
+        {
+            return name + ", " + pages + " pages, " + year;
+        }
+    }
+}
+
+
+
+
--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -7,9 +7,38 @@ namespace exercise_113
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      // Remember also to create the file Book.cs and class in it!
+        List<Book> list = new List<Book>();
 
+        while(true)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            
+            if(name == "") {break;}
+
+            Console.Write("Pages: ");
+            int pages = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Publication year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            list.Add(new Book(name, pages, year));
+        }
+        Console.WriteLine();
+        
+        Console.Write("What informaton will be printed? ");
+        string answer = Console.ReadLine();
+
+        if(answer == "everything")
+        {
+            list.ForEach(Console.WriteLine);
+        }
+        else if(answer == "title")
+        {
+            foreach(Book info in list)
+            {
+                Console.WriteLine(info.name);
+            }
+        }
     }
   }
 }
