4f9e91e477f405682d948c94271e57f8b224babb
Jussi <jussiottopesonen@gmail.com>
2021-03-09 16:05:38 +0200

    9.3.2021

---
+++ Exercises/Part 4/Exercise 106/PaymentCard.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace exercise_106
+{
+    class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            // write code here
+            this.balance = openingBalance;
+        }
+
+        public override string ToString()
+        {
+            // write code here
+            return "The card has a balance of " + balance + " euros";
+        }
+    }
+}
+
+
+
