0bfea2376f175044ed687954b4102268e7a26de5
Jussi <jussiottopesonen@gmail.com>
2021-03-09 15:21:22 +0200

    9.3.2021

--- Exercises/Part 4/Exercise 098/Product.cs
+++ Exercises/Part 4/Exercise 098/Product.cs
@@ -4,20 +4,20 @@ namespace exercise_98
 {
   class Product
   {
-        public double priceX;
-        public int quantityX;
-        public string nameX;
+        public double price;
+        public int quantity;
+        public string name;
 
         public Product(string name, double price, int quantity)
         {
-            this.priceX = price;
-            this.quantityX = quantity;
-            this.nameX = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.name = name;
         }
 
         public void PrintProduct()
         {
-            Console.WriteLine(nameX + ": price " + priceX + ": " + quantityX + " pcs");
+            Console.WriteLine(name + ": price " + price + ": " + quantity + " pcs");
         }
   }
 }
