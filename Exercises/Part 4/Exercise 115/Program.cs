8fd9c77751bcdb1b0431219034aea5961834bda3
Jussi <jussiottopesonen@gmail.com>
2021-03-10 14:31:21 +0200

    10.3.2021

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+            while(true)
+            {
+                string text = Console.ReadLine();
 
+                if(text == "end") { break; }
+
+                int number = Convert.ToInt32(text);
+
+                int cube = number * number * number;
+                
+                Console.WriteLine(cube);
+            }
     }
   }
 }
