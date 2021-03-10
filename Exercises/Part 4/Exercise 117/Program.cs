a89820cc8be3702d0945c363e4916e68e9c9e59b
Jussi <jussiottopesonen@gmail.com>
2021-03-10 15:04:31 +0200

    10.3.2021

--- Exercises/Part 4/Exercise 117/Program.cs
+++ Exercises/Part 4/Exercise 117/Program.cs
@@ -8,8 +8,16 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
+        
+        Console.WriteLine("Which file should have its contents printed?");
+        string name = Console.ReadLine();
+        Console.WriteLine();
+        string[] lines = File.ReadAllLines(name);
 
-      
+        foreach(string line in lines)
+        {
+            Console.WriteLine(line);
+        }
     }
   }
 }
