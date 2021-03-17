c3d91fcb9891b6ba31dd54aecbf0b88621d93e7e
Jussi <jussiottopesonen@gmail.com>
2021-03-17 16:24:02 +0200

    17.3.2021

--- Exercises/Part 5/Exercise 130/Book.cs
+++ Exercises/Part 5/Exercise 130/Book.cs
@@ -16,9 +16,22 @@ namespace exercise_130
 
 
     // BEGIN SOLUTION
-    public bool Equals(Book compared)
+    public override bool Equals(object compared)
     {
-        if(this.name == compared.name & this.publicationYear == compared.publicationYear)
+        if(this == compared)
+        {
+            return true;    
+        }  
+
+        if(compared == null || !this.GetType().Equals(compared.GetType()))
+        {
+            return false;
+        }
+        
+        Book comparedBook = (Book)compared;
+
+        if(this.name == comparedBook.name & 
+           this.publicationYear == comparedBook.publicationYear)
         {
             return true;
         }
