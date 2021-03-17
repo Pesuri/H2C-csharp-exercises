22ee50463fad8421b8bd54262f88343bfed0dbce
Jussi <jussiottopesonen@gmail.com>
2021-03-17 16:02:38 +0200

    17.3.2021

--- Exercises/Part 5/Exercise 133/Money.cs
+++ Exercises/Part 5/Exercise 133/Money.cs
@@ -20,8 +20,15 @@ namespace exercise_133
 
     public Money Plus(Money addition)
     {
-      Money newMoney = new Money(/* Do something here*/);
+      Money newMoney = new Money(this.euros, this.cents);
       // create a new Money object that has the correct worth
+      newMoney.euros += addition.euros;
+      newMoney.cents += addition.cents;
+      while(cents >= 100)
+      {
+        newMoney.cents -= 100;
+        newMoney.euros++;
+      }
 
       // return the new Money object
       return newMoney;
@@ -29,8 +36,21 @@ namespace exercise_133
 
     public Money Minus(Money decreaser)
     {
-      Money newMoney = new Money(/* Do something here*/);
+      Money newMoney = new Money(this.euros, this.cents);
       // create a new Money object that has the correct worth
+      newMoney.euros -= decreaser.euros;
+      newMoney.cents -= decreaser.cents;
+      while(cents < 0)
+      {
+        newMoney.cents += 100;
+        newMoney.euros--;
+      }
+
+      if(newMoney.euros < 0)
+      {
+          newMoney.euros = 0;
+          newMoney.cents = 0;
+      }
 
       // return the new Money object
       return newMoney;
@@ -38,7 +58,9 @@ namespace exercise_133
 
     public bool LessThan(Money compared)
     {
-      // Do something here
+      double value1 = this.euros + this.cents/100;
+      double value2 = compared.euros + compared.cents/100;
+      if(value1 < value2) {return true;}
       return false;
     }
 
